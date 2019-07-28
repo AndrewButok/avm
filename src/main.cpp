806a47fe4b0c611880cdc3c5520c62d4ee1fcf25
abutok <andrewbutok@ukr.net>
2019-07-28 13:12:00 +0300

    Merge branch 'master' of /Users/abutok/Documents/avm with conflicts.

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,29 @@
 /*   By: abutok <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 13:58:33 by abutok            #+#    #+#             */
-/*   Updated: 2019/07/26 23:49:21 by abutok           ###   ########.fr       */
+/*   Updated: 2019/07/25 17:39:30 by abutok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cmath>
 #include <vector>
-#include "Lexer.hpp"
+#include <sstream>
+#include <Token.hpp>
 #include "Executor.hpp"
 
 int main() {
-	std::string s("abcd dlp\t");
-	Lexer::tokenize(s);
+	OperandFactory *of = OperandFactory::getInstance();
+	const IOperand *f1 = of->createOperand(IOperand::eOperandType::Int8, "45"),
+			*f2 = of->createOperand(IOperand::eOperandType::Int8, "25");
+	Executor *executor = Executor::getInstance();
+	executor->pushToStack(f1);
+	executor->pushToStack(f2);
+	executor->popFromStack();
+	executor->pushToStack(f1);
+	executor->add();
+	executor->dumpStack();
+	executor->print();
+	system("leaks Abstract-VM");
 	return 0;
 }
